6bd3f9e05c34cb63ea927424aee796c1ce2dc390
bri-yan <bri.yan@outlook.com>
2021-10-27 16:12:35 -0700

    lab6

---
+++ gazebo_env_lab06.py
@@ -0,0 +1,196 @@
+import cv2
+import gym
+import math
+import rospy
+import roslaunch
+import time
+import numpy as np
+
+from cv_bridge import CvBridge, CvBridgeError
+from gym import utils, spaces
+from gym_gazebo.envs import gazebo_env
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Empty
+
+from sensor_msgs.msg import Image
+from time import sleep
+
+from gym.utils import seeding
+
+
+class Gazebo_Lab06_Env(gazebo_env.GazeboEnv):
+
+    def __init__(self):
+        # Launch the simulation with the given launchfile name
+        LAUNCH_FILE = '/home/fizzer/enph353_gym-gazebo/gym_gazebo/envs/ros_ws/src/enph353_lab06/launch/lab06_world.launch'
+        gazebo_env.GazeboEnv.__init__(self, LAUNCH_FILE)
+        self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+        self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_world',
+                                              Empty)
+
+        self.action_space = spaces.Discrete(3)  # F,L,R
+        self.reward_range = (-np.inf, np.inf)
+        self.episode_history = []
+
+        self._seed()
+
+        self.bridge = CvBridge()
+        self.timeout = 0  # Used to keep track of images with no line detected
+
+        self.lower_blue = np.array([97,  0,   0])
+        self.upper_blue = np.array([150, 255, 255])
+
+    def process_image(self, data):
+        '''
+            @brief Coverts data into a opencv image and displays it
+            @param data : Image data from ROS
+
+            @retval (state, done)
+        '''
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
+        except CvBridgeError as e:
+            print(e)
+
+        # cv2.imshow("raw", cv_image)
+
+        state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        done = False
+
+        (height, width) = cv_image.shape    
+        element_width = width // 10
+        _, thresh = cv2.threshold(cv_image, 120, 255, cv2.THRESH_BINARY_INV)
+        thresh = thresh[3*height//4:]   
+        if np.count_nonzero(thresh) < 100:
+            self.timeout += 1
+            done = self.timeout > 20
+            return state, done
+        try:
+            x_bar, y_bar = self.find_center(thresh)
+        except ZeroDivisionError as e:
+            self.timeout += 1
+            done = self.timeout > 30
+            return state, done
+        self.timeout = 0
+        element_index = x_bar // element_width
+        state[element_index] = 1
+        cv2.circle(cv_image, (x_bar, 3*height//4+y_bar), 10, 255, -1)
+        cv2.imshow("Robot Feed", cv_image)
+        cv2.waitKey(3)
+        return state, done
+    
+    def find_center(self, thresh):
+        M = cv2.moments(thresh)
+        x_bar = int(M["m10"] / M["m00"])
+        y_bar = int(M["m01"] / M["m00"])
+        return x_bar, y_bar
+
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, action):
+
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            self.unpause()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/unpause_physics service call failed")
+
+        self.episode_history.append(action)
+
+        vel_cmd = Twist()
+
+        if action == 0:  # FORWARD
+            vel_cmd.linear.x = 0.25
+            vel_cmd.angular.z = 0.0
+        elif action == 1:  # LEFT
+            vel_cmd.linear.x = 0.0
+            vel_cmd.angular.z = 0.5
+        elif action == 2:  # RIGHT
+            vel_cmd.linear.x = 0.0
+            vel_cmd.angular.z = -0.5
+
+        self.vel_pub.publish(vel_cmd)
+
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('/pi_camera/image_raw', Image,
+                                              timeout=5)
+            except:
+                pass
+
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            # resp_pause = pause.call()
+            self.pause()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/pause_physics service call failed")
+
+        state, done = self.process_image(data)
+
+        # Set the rewards for your action
+        bias = 10 * state == 4
+        anti = 0
+        if len(self.episode_history) > 40:
+            if 0 not in self.episode_history[-40:]:
+                done = True
+
+
+        if not done:
+            if action == 0:  # FORWARD
+                reward = 4 #+ bias
+            elif action == 1:  # LEFT
+                reward = 2 #- anti
+            else:
+                reward = 2 #- anti # RIGHT
+        else:
+            reward = -200
+
+        return state, reward, done, {}
+
+    def reset(self):
+
+        print("Episode history: {}".format(self.episode_history))
+        self.episode_history = []
+        print("Resetting simulation...")
+        # Resets the state of the environment and returns an initial
+        # observation.
+        rospy.wait_for_service('/gazebo/reset_simulation')
+        try:
+            # reset_proxy.call()
+            self.reset_proxy()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/reset_simulation service call failed")
+
+        # Unpause simulation to make observation
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            # resp_pause = pause.call()
+            self.unpause()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/unpause_physics service call failed")
+
+        # read image data
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('/pi_camera/image_raw',
+                                              Image, timeout=5)
+            except:
+                pass
+
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            # resp_pause = pause.call()
+            self.pause()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/pause_physics service call failed")
+
+        self.timeout = 0
+        state, done = self.process_image(data)
+
+        return state
